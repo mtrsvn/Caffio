75110e4d610dbdb5e5f63764fa131ff083832419
mtr <matemasaalunga@gmail.com>
2026-06-26 20:58:50 +0800

    security: move secrets to .env, add .env.example templates

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -30,14 +30,13 @@ import {
 } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBn03Xi_UWe0Ca8sTvSMOoViHwttcHwA0",
-  authDomain: "caffio-291e7.firebaseapp.com",
-  projectId: "caffio-291e7",
-
-  storageBucket: "caffio-291e7.firebasestorage.app",
-  messagingSenderId: "137243555767",
-  appId: "1:137243555767:web:3cb68cc1f4d6696f0a5a32",
-  measurementId: "G-5K4VKJ8SV7",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 let app;
